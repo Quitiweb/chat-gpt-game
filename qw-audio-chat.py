13fcd2348a03692db6a0139d644db2a11c6be6ac
Rafael Ruiz <r.ruiz@cathedralsw.com>
2023-05-16 17:12:42 +0200

    add an audio recorder and play the sound afterwards

--- qw-audio-chat.py
+++ qw-audio-chat.py
@@ -1,3 +1,15 @@
-from tools.qw_whisper import audio_to_text
+from playsound import playsound
 
-my_file = "media/audio.mp3"
+from tools.qw_whisper import audio_to_text
+from tools.record_audio import record_audio_to_file
+from tools.text_to_speech import text_to_audio_file
+
+my_file = "media/recorded.wav"
+audio_path = "media/audio_speech_v1.mp3"
+
+record_audio_to_file(my_file)
+
+text = audio_to_text(my_file)
+text_to_audio_file(text, audio_path)
+
+playsound(audio_path)
--- tools/text_to_speech.py
+++ tools/text_to_speech.py
@@ -1,26 +1,30 @@
 import os
 from google.cloud import texttospeech_v1
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "media/qw-text-to-speech-97614f4c7ac4.json"
-client = texttospeech_v1.TextToSpeechClient()
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "media/qw-text-to-speech-d985f90a84c0.json"
 
-text = "<speak>Hola. Qué tal estás?</speak>"
-synthesis_input = texttospeech_v1.SynthesisInput(ssml=text)
 
-voice1 = texttospeech_v1.VoiceSelectionParams(
-    language_code="es-es",
-    # name="eu-ES-Standard-A",
-    # ssml_gender=texttospeech_v1.SsmlVoiceGender.FEMALE
-)
+def text_to_audio_file(text_in, audio_path):
+    client = texttospeech_v1.TextToSpeechClient()
+    file_path = audio_path
 
-# print(client.list_voices())
-audio_config = texttospeech_v1.AudioConfig(audio_encoding=texttospeech_v1.AudioEncoding.MP3)
+    text = "<speak>" + text_in + "</speak>"
+    synthesis_input = texttospeech_v1.SynthesisInput(ssml=text)
 
-response = client.synthesize_speech(
-    input=synthesis_input,
-    voice=voice1,
-    audio_config=audio_config
-)
+    voice1 = texttospeech_v1.VoiceSelectionParams(
+        language_code="es-es",
+        # name="eu-ES-Standard-A",
+        # ssml_gender=texttospeech_v1.SsmlVoiceGender.FEMALE
+    )
 
-with open("media/audio_speech_v2.mp3", "wb") as output:
-    output.write(response.audio_content)
+    # print(client.list_voices())
+    audio_config = texttospeech_v1.AudioConfig(audio_encoding=texttospeech_v1.AudioEncoding.MP3)
+
+    response = client.synthesize_speech(
+        input=synthesis_input,
+        voice=voice1,
+        audio_config=audio_config
+    )
+
+    with open(file_path, "wb") as output:
+        output.write(response.audio_content)
