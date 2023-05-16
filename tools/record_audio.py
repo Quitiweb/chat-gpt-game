70748ed0416633be961ed83d6758e170d6db087f
Rafael Ruiz <r.ruiz@cathedralsw.com>
2023-05-16 21:41:13 +0200

    last test completed

--- tools/record_audio.py
+++ tools/record_audio.py
@@ -6,44 +6,54 @@ def record_audio_to_file(filename):
     # set the chunk size of 1024 samples
     chunk = 1024
     # sample format
-    FORMAT = pyaudio.paInt16
+    sformat = pyaudio.paInt16
     # mono, change to 2 if you want stereo
     channels = 1
     # 44100 samples per second
     sample_rate = 44100
-    record_seconds = 5
-    # initialize PyAudio object
+    record_seconds = 10
+
     p = pyaudio.PyAudio()
-    # open stream object as input & output
-    stream = p.open(format=FORMAT,
+    stream = p.open(format=sformat,
                     channels=channels,
                     rate=sample_rate,
                     input=True,
                     output=True,
                     frames_per_buffer=chunk)
     frames = []
+
+    print()
     print("Recording...")
+    print()
+    print("Seconds left: 10")
+    partial_time = 42
+    seconds_left = 9
+
     for i in range(int(sample_rate / chunk * record_seconds)):
+        if i > partial_time:
+            print(f"Seconds left: {seconds_left}")
+            partial_time += 42
+            seconds_left -= 1
         data = stream.read(chunk)
+
         # if you want to hear your voice while recording
         # stream.write(data)
         frames.append(data)
+
+    print()
     print("Finished recording.")
-    # stop and close stream
+    print()
+
     stream.stop_stream()
     stream.close()
-    # terminate pyaudio object
     p.terminate()
+
     # save audio file
     # open the file in 'write bytes' mode
     wf = wave.open(filename, "wb")
-    # set the channels
     wf.setnchannels(channels)
-    # set the sample format
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    # set the sample rate
-    wf.setframerate(sample_rate)
-    # write the frames as bytes
-    wf.writeframes(b"".join(frames))
-    # close the file
+    wf.setsampwidth(p.get_sample_size(sformat))  # set the sample format
+    wf.setframerate(sample_rate)  # set the sample rate
+    wf.writeframes(b"".join(frames))  # write the frames as bytes
+
     wf.close()
